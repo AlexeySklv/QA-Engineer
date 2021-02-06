9d53504af8e5d89c21d0c9d1cb14e9420d43d051
AlexeySkvl <baradada12@yandex.ru>
2021-02-06 20:24:00 +0300

    javacore2

---
+++ geekbrainz/DZ2/src/main/java/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[][] arr = new String[][]{{"3", "2","X", "6"}, {"5", "7", "1", "8"}, {"2", "3", "5", "9"}, {"8", "4", "5", "2"}};
+
+        try {
+            try {
+                int result = method(arr);
+                System.out.println(result);
+            } catch (MyArraySizeException e) {
+                System.out.println("Размер превышен!");
+            }
+        } catch (MyArrayDataException e) {
+            System.out.println("Неправильное значение");
+            System.out.println("Ошибка в ячейке: " + (e.i) + " - " + (e.j));
+        }
+
+    }
+
+
+    public static int method(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int count = 0;
+        if (arr.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    count = count + Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+
+        }
+        return count;
+    }
+
+}
